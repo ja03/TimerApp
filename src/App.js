2ec8e84a1bac6765a529b7d2b26b2bc3c417311d
Ahmad Mahmoud <abuhamdiehahmad@gmail.com>
2023-09-17 23:13:36 +0300

    version 1.2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
 import Timer from "./Timer";
+import { useState } from "react";
+import Logo from './clockWiseLogo.svg'
 
 function App() {
+  const [bgColor, setBgColor] = useState('#3D7C91')
+  // const [textColor , setTextColor] = useState('#ffffff')
+
+  // useEffect(()=>{
+  //   if(bgColor === '#BFD1E5'){
+  //     setTextColor('#121212')
+  //   }else {
+  //     setTextColor('#ffffff')
+  //   }
+  // },[bgColor, textColor])
+
   return (
-    <div className="mx-auto py-20 bg-[#3D7C91] w-full h-screen ">
-      <h1 className="text-center text-3xl text-white">Timer Application</h1>
-      <Timer />
+    <div className={`py-10 bg-[${bgColor}] w-full h-auto md:h-screen relative transition-colors`}>
+      <div className=" grid items-center">
+        <div className="flex gap-4 items-center px-8"> 
+          <img src={Logo} alt="logo" className={ `w-[32px]` }/>
+          <h1 className={`text-center text-3xl text-[#fff]`}>ClockWise</h1> 
+        </div>
+        <Timer fontColor={'#fff'}/>
+        <div className="md:absolute right-5 bottom-5 flex md:flex-col flex-row md:gap-2 mx-auto gap-10 my-4">
+          <button
+              className="rounded-md w-8 h-8 bg-[#3D7C91] cursor-pointer shadow-md shadow-slate-600 border-2 border-slate-600/50"
+              onClick={()=>{setBgColor('#3D7C91')}}
+          ></button>
+          <button
+              className="rounded-md w-8 h-8 bg-[#373F51] cursor-pointer shadow-md shadow-slate-600 border-2 border-slate-600/50"
+              onClick={()=>{setBgColor('#373F51')}}
+          ></button>
+          <button
+              className="rounded-md w-8 h-8 bg-[#B91372] cursor-pointer shadow-md shadow-slate-600 border-2 border-slate-600/50"
+              onClick={()=>{setBgColor('#B91372')}}
+          ></button>
+          <bytton
+              className="rounded-md w-8 h-8 bg-[#95A3B2] cursor-pointer shadow-md shadow-slate-600 border-2 border-slate-600/50"
+              onClick={()=>{setBgColor('#95A3B2')}}
+          ></bytton>
+        </div>
+      </div>
     </div>
   );
 }
--- src/Timer.js
+++ src/Timer.js
@@ -1,34 +1,41 @@
 import React, { useEffect } from 'react'
 import { useState } from 'react'
-const Timer = ()=>{
+const Timer = ({fontColor})=>{
     const [count, setCount]= useState(0)
     const [sec, setSec]= useState(0)
-    const [min, setMin] = useState(0)
-    const [inputValue, setInputValue]= useState(' ')
+    const [min, setMin] = useState(25)
+    const [inputValue, setInputValue]= useState('')
     const [play, setPlay] = useState(true)
     const [session, setSession]= useState(-1)
     // const [shortBreak, setShortBreak] = useState(false)
 
-    const countDown = ()=>{
-            setMin(Math.floor(count/60))
-            setSec(count % 60)
-            setCount(count-1)
-            count === 0 ? setSession(session+1) : console.log('not yet')
-    }
-
+    
     useEffect( () => {
+        const countDown = ()=>{
+                setMin(Math.floor(count/60))
+                setSec(count % 60)
+                setCount(count-1)
+                count === 0 ? setSession(session+1) : console.log('not yet')
+        }
         if(count >= 0 && play){
             const interval = setInterval(countDown, 1000)
             return ()=>{clearInterval(interval)}
         }
-    },[count, play, sec, countDown])
+    },[count, play, sec, session])
     const getTime = (e) => {
         e.preventDefault();
         if(inputValue > 0){
             const newCount = parseInt(inputValue, 10);
             setCount(newCount*60);
         }
-    };
+    }
+    const startTimer= ()=>{
+        if(session > 0 && session % 4 === 0){
+            setCount(5*60)
+        }else{
+            setCount(25*60)
+        }
+    }
     const stopTimer = ()=>{
         setPlay(false)
     }
@@ -37,30 +44,36 @@ const Timer = ()=>{
     }
     const skipSession = ()=>{
         setCount(0)
-        setSession(session+1)
     }
     return(
-        <div className='mx-auto grid items-center text-white '>
-            <div className='mx-auto my-20 flex flex-col items-center gap-4'>
-                <h2 className='text-6xl font-semibold tracking-wider'>
-                    {count > 0 ? `${String(min).padStart(2, '0')}:${String(sec).padStart(2, '0')}` : '00:00'}
+        <div className={`mx-auto grid items-center text-[${fontColor}] transition-colors p-2`}>
+            <div className='mx-auto my-10 rounded-lg border-4 border-white/50 w-full h-60 flex flex-col items-center justify-center gap-4  '>
+                <h2 className='text-7xl font-semibold tracking-wider'>
+                    {count > 0 ? `${String(min).padStart(2, '0')}:${String(sec).padStart(2, '0')}` : '25:00'}
                 </h2>
-                <p>{session > 0 ? `Finished Sessions: ${session}`: `Finished Sessions: 0`}</p>
-                <p>{session === 4 ? 'Break Time' : ' '}</p>
+                <p>{session > 0 ? `Completed  Sessions: ${session}`: `Completed Sessions: 0`}</p>
+                <p>{session > 0 && session % 4 === 0? `Break Time ` : ' '}</p>
             </div>
             <div className='mx-auto flax flex-col space-y-7'>
-                <form onSubmit={getTime}>
-                    <button className='border-black bg-slate-600 mx-4 p-2 rounded text-cyan-200' type='submit'>Set Timer</button> 
+                <div>
+                    <button className='w-full border-black bg-slate-600 p-2 rounded text-cyan-200 mx-auto' onClick={startTimer}>
+                        {session > 0 && session % 4 === 0 ?  'Start Short Break' : 'Start Timer'}
+                    </button>
+                </div>
+                <div className='w-full flex flex-col md:flex-row gap-4' >
+                    <button className='border-black bg-slate-600 p-2 rounded text-cyan-200 md:w-[100px] w-full' onClick={stopTimer}>Pause</button>
+                    <button className='border-black bg-slate-600 p-2 rounded text-cyan-200 md:w-[100px] w-full' onClick={resumeTimer}>Resume</button>
+                    <button className='border-black bg-slate-600 p-2 rounded text-cyan-200 md:w-[100px] w-full' onClick={skipSession}>Skip </button>
+                </div>
+                <form className='w-full' onSubmit={getTime}>
+                    <button className='border-black bg-slate-600 mr-4 p-2 rounded text-cyan-200' type='submit'>Set Timer</button> 
                     <input 
                         type="number" 
-                        className='mx-4 p-2  text-cyan-800 bg-teal-400 rounded-md opacity-40 hover:opacity-100 transition-opacity outline-0' 
+                        className='ml-4 p-2  text-cyan-800 bg-teal-400 rounded-md opacity-40 hover:opacity-100 transition-opacity outline-0 w-1/3' 
                         value={inputValue}
                         onChange={(e)=>{setInputValue(e.target.value)}}
                     />
                 </form>
-                <button className='border-black bg-slate-600 mx-4 p-2 rounded text-cyan-200' onClick={stopTimer}>Pause</button>
-                <button className='border-black bg-slate-600 mx-4 p-2 rounded text-cyan-200' onClick={resumeTimer}>Play Timer</button>
-                <button className='border-black bg-slate-600 mx-4 p-2 rounded text-cyan-200' onClick={skipSession}>Skip session</button>
             </div>
         </div>
     )
